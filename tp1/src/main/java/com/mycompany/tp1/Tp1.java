37df4352cb67601f7c76b86fd144185a67c60eb9
SirGluten <lyallnicolas@gmail.com>
2023-08-28 14:57:49 -0300

    tp1 commited

---
+++ tp1/src/main/java/com/mycompany/tp1/Tp1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp1;
+
+/**
+ *
+ * @author nicol
+ */
+public class Tp1 {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(()->{new Vista().setVisible(true);});
+       // System.out.println("Hello World!");
+        
+    }
+}
+
+
